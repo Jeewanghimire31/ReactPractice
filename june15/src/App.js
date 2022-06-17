9724b1823290e09d3ba86a34e7763ff83dc60998
jeewan <jeewanghimire31@gmail.com>
2022-06-17 18:34:42 +0545

    Importing Bootstrap

---
+++ june15/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+//  import "bootstrap/dist/css/bootstrap.css"
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import { Button } from 'react-bootstrap';
+ import React from 'react';
+ 
+function App() {
+  return (
+    <>
+      <Button> Test </Button>
+    </>
+    
+  );
+}
+
+export default App;
